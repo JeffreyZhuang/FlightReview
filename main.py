d437bf0607a72fd14c5d8216468bfaacbf990a03
JeffreyZhuang <jeffreyzhuangyt@gmail.com>
2025-02-16 14:49:23 -0500

    a

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import struct
+from cobs import cobs
 
-struct_format = 'Q2f'
+struct_format = 'II'
 
-file = open('log1.bin', 'rb')
+file = open('log115.bin', 'rb')
 
 while True: 
-    data = file.read(struct.calcsize(struct_format))
-    unpacked_data = struct.unpack(struct_format, data)
-    print(unpacked_data)
+    byte = file.read(1)
+
+    # Find start byte
+    if byte == b'\x00':
+        packet_cobs = file.read(struct.calcsize(struct_format) + 1) # Add 1 because COBS byte
+        packet = cobs.decode(packet_cobs)
+        data = struct.unpack(struct_format, packet)
+        print(data)
